bbef2e810e5ccc559e679c36b546c7d900358de5
Vitaliy Tishik <vitisik54@gmail.com>
2025-11-26 18:49:06 +0200

    ref

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -9,7 +9,8 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
-    private HibernateUtil() {}
+    private HibernateUtil() {
+    }
 
     private static SessionFactory buildSessionFactory() {
         try {
